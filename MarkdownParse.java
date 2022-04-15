face3e5c548c8daf0f76ed6ac7ef8ebc610048f9
fnriv <fnrivera@ucsd.edu>
2022-04-14 21:07:21 -0700

    Finished tests
    had to check if index was negative first before making the changes

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -10,15 +10,24 @@ public class MarkdownParse {
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
+
         int currentIndex = 0;
+       
         while(currentIndex < markdown.length()) {
+            
             int openBracket = markdown.indexOf("[", currentIndex);
+            if (openBracket < 0){break;}
+
             int closeBracketAndOpenParentheses = markdown.indexOf("](", openBracket);
+            if (closeBracketAndOpenParentheses < 0){break;}
+
             int closeParen = markdown.indexOf(")", closeBracketAndOpenParentheses);
+            if(closeParen < 0){break;}
+
             toReturn.add(markdown.substring(closeBracketAndOpenParentheses + 2, closeParen));
             currentIndex = closeParen + 1;
+        
         }
-
         return toReturn;
     }
 
